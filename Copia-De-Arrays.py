e0ab89112d5a8b1c63d4b3b0fdab3b7997d65df5
Luis <luismarfran08@gmail.com>
2025-07-11 18:57:01 -0600

    a

--- Copia-De-Arrays.py
+++ Copia-De-Arrays.py
@@ -1,7 +1,10 @@
 import numpy as np
 precio_inmuebles = np.array([10000, 120000, 11000, 200000])
-#precio_inmuebles_lima = precio_inmuebles
-precio_inmuebles_lima = np.copy(precio_inmuebles)
+
+#precio_inmuebles_lima = precio_inmuebles       Forma incorrecta
+precio_inmuebles_lima = np.copy(precio_inmuebles) # Forma correcta
+
 precio_inmuebles[0] = 120000
+
 print(precio_inmuebles)
 print(precio_inmuebles_lima)
